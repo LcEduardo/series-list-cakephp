65ad8f8f9590cf69f637f499df0577058f8e4195
LcEduardo <lucasproject777@gmail.com>
2026-06-13 15:37:02 -0300

    feat: criação da tabela que mostra os usuários + botão que leva ao formulário de cadastro de usuário

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -14,4 +14,22 @@ class UsersController extends AppController
 
         $this->set(compact('users'));
     }
+
+    public function add()
+    {
+        $user = $this->Users->newEmptyEntity();
+
+        if ($this->request->is('post')) {
+            $user = $this->Users->patchEntity($user, $this->request->getData());
+
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('User has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to add user. Please try again.'));
+        }
+
+        $this->set(compact('user'));
+    }
 }
